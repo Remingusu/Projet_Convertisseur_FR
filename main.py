7aa5f44e1464873c68b86166eae0c7b3e9b1357d
Remingusu <leween.m33@gmail.com>
2022-10-13 09:41:25 +0200

    add error manage

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ enter_base = int(input("Base d'entrée: "))
 exit_base = int(input("Base de sortie: "))
 nbr = int(input("Nombre à convertir en base", enter_base))
 
+if not enter_base==2 or not enter_base==10 or not enter_base==16:
+    print("Cette base n'est pas conforme.")
