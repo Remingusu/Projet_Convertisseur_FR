a2fa014313fd3c5fd9aa10f9ccb6e8f8f443ba22
Remingusu <leween.m33@gmail.com>
2022-10-13 09:55:08 +0200

    Co-authored-by: gamingtira@gmail.com <gamingtira@gmail.com> Co-authored-by: hicham.veret@gmail.com <hicham.veret@gmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 numtoconvert = input("Enter a number to convert from base 16 to base 2 : ")
 
-for charac in numtoconvert:
-    print(charac)
+for num in numtoconvert:
+    print(num)
 
 print(f"Taille : {len(numtoconvert)}")
