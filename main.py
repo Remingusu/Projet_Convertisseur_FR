512af77afb325f45d1569f38ed5e65252dfc2ec5
Remingusu <leween.m33@gmail.com>
2022-10-15 11:49:06 +0200

    Rapport Technique rapport techniques + tentative verification base 2, 10 ou 16 (à finir)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-enter_base = int(input("Base d'entrée: ")) # demande la base du nombre
-exit_base = int(input("Base de sortie: ")) # demande la base dans laquel le nombre va être convertis
-nbr = input(f"Nombre à convertir en base {enter_base}: ") # demande le nombre
-num_bin = "" # def 'num_bin' pour qu'il ne soit pas indéfini
+loop = True # déf loop sur True
 
-if (enter_base != 2 or 10 or 16) or (exit_base != 2 or 10 or 16):
-    print("Cette base n'est pas conforme.") # afficher la phrase
+while loop: # tant que loop n'est pas faux alors:
+    enter_base = int(input("Base d'entrée (aucun effet): ")) # demande la base du nombre
+    exit_base = int(input("Base de sortie (aucun effet): ")) # demande la base dans laquel le nombre va être converti
+    if 2 or 10 or 16 in  {enter_base and exit_base}:# vérification si la base d'entrée et de sortis est 2, 10 ou 16
+        loop=False # si oui, sortir de la boucle
+    else:
+        print("Une des bases n'est pas conforme.") # si non, afficher la phrase
+
+nbr = input(f"Nombre en base 16 à convertir en base 2: ") # demande le nombre
+nbr_bin = "" # def 'num_bin' pour qu'il ne soit pas indéfini
 
 dico={ '0': '0000',
        '1': '0001',
@@ -27,8 +32,8 @@ for i in range(len(nbr)): # boucle 'for' de la taille du nombre donner au dépar
     index = nbr[i] # def 'index' avec le charactère du nombre du début par rapport à l'incrémentation
     if 97 <= ord(index) <= 122: # verification si le code décimal ascci du charactère est entre 97 et 122 (charactères minuscules)
         index = chr(ord(index)-32) # si oui, on retire 32 pour mettre le charactère en majuscule
-        num_bin = num_bin + dico[index] + " " # concaténation du code binaire précédent avec le code binaire du characètre dont la valeur est définie dans le dictionnaire
+        nbr_bin = nbr_bin + dico[index] + " " # concaténation du code binaire précédent avec le code binaire du characètre dont la valeur est définie dans le dictionnaire
     elif 48 <= ord(index) <= 57 or 65 <= ord(index) <= 90: # vérification si le charactère est entre 48 et 57 ou 65 et 90 (majuscule ou nombre)
-        num_bin = num_bin + dico[index] + " " # concaténation du code binaire précédent avec le code binaire du characètre dont la valeur est définie dans le dictionnaire
+        nbr_bin = nbr_bin + dico[index] + " " # concaténation du code binaire précédent avec le code binaire du characètre dont la valeur est définie dans le dictionnaire
 
-print("Nombre converti en base 2:", num_bin) # afficher la phrase + le nombre converti
+print("Nombre converti en base 2:", nbr_bin) # afficher la phrase + le nombre converti
